8ca8b28e4c272d39dc9c20c265bd211445081a3e
ikase-comm <124385023+ikase-comm@users.noreply.github.com>
2023-07-25 12:14:09 +0200

    new file:   .gitignore 	new file:   LICENSE 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/assets/audio/game-over.mp3 	new file:   src/assets/audio/mario-died.mp3 	new file:   src/assets/audio/mario-jump.mp3 	new file:   src/assets/audio/running-about.mp3 	new file:   src/assets/birds.gif 	new file:   src/assets/brick.svg 	new file:   src/assets/clouds.gif 	new file:   src/assets/font/Pixel_NES.otf 	new file:   src/assets/goomba.gif 	new file:   src/assets/koopa.gif 	new file:   src/assets/mario-run.gif 	new file:   src/assets/mario.png 	new file:   src/assets/sun.gif 	new file:   src/components/Birds.css 	new file:   src/components/Birds.js 	new file:   src/components/Bricks.css 	new file:   src/components/Bricks.js 	new file:   src/components/Clouds.css 	new file:   src/components/Clouds.js 	new file:   src/components/Footer.css 	new file:   src/components/Footer.js 	new file:   src/components/KeyMessage.css 	new file:   src/components/KeyMessage.js 	new file:   src/components/LoadingScreen.css 	new file:   src/components/LoadingScreen.js 	new file:   src/components/Mario.css 	new file:   src/components/Mario.js 	new file:   src/components/MobileControls.css 	new file:   src/components/MobileControls.js 	new file:   src/components/Obstacles.css 	new file:   src/components/Obstacles.js 	new file:   src/components/Score.css 	new file:   src/components/Score.js 	new file:   src/components/Sun.css 	new file:   src/components/Sun.js 	new file:   src/components/Title.css 	new file:   src/components/Title.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/redux/engineSlice.js 	new file:   src/redux/marioSlice.js 	new file:   src/redux/obstacleSlice.js 	new file:   src/redux/store.js

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import Birds from "./components/Birds";
+import Bricks from "./components/Bricks";
+import Clouds from "./components/Clouds";
+import Mario from "./components/Mario";
+import Obstacles from "./components/Obstacles";
+import Sun from "./components/Sun";
+import KeyMessage from "./components/KeyMessage";
+import LoadingScreen from "./components/LoadingScreen";
+
+// redux
+import { useSelector } from "react-redux";
+import Score from "./components/Score";
+import MobileControls from "./components/MobileControls";
+import Footer from "./components/Footer";
+
+function App() {
+  const isPlay = useSelector((state) => state.engine.play);
+  const isLoading = useSelector((state) => state.engine.loadingScreen);
+  return (
+    <>
+      {isLoading && <LoadingScreen />}
+      <div className="App">
+        {!isPlay && <KeyMessage />}
+        <Bricks />
+        <Mario />
+        <Sun />
+        <Clouds />
+        <Birds />
+        <Obstacles />
+        <Score />
+      </div>
+      <MobileControls />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
